5301ef1a4c875b9d05d8ec0bd0cc47bce5afb4f0
izadoranetz <izadora.ns@gmail.com>
2023-07-10 19:29:32 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const date = require(__dirname + '/date.js');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+let items = ['Groceries', 'Study Math'];
+let workItems = [];
+
+app.get('/', function(req,res){
+    let day = date();
+    
+    res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post('/', function(req,res){
+
+    let item = req.body.newItem;
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect('/work');
+     } else {
+         items.push(item);
+         res.redirect('/');
+     }
+    
+});
+
+app.get('/work', function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post('/work', function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect('/work');
+});
+
+app.get('/about', function(req, res){
+    res.render('about');
+})
+
+app.listen(3000, function(){
+    console.log('Server is running on port 3000');
+});
